141659f800a8d7848e095ef1f345e634c8dd3106
Stanley <stanley@stanographer.com>
2019-04-13 14:03:59 -0400

    Mongo/Redis networking fix.

--- connections.js
+++ connections.js
@@ -2,14 +2,14 @@
 
 const mongo = require('sharedb-mongo')(
   process.env.NODE_ENV === 'production'
-    ? 'mongodb://165.227.187.41:27017/upwordly'
-    : 'mongodb://localhost:27017/aloft'
+    ? `mongodb://${ process.env.MONGO_IP }/upwordly`
+    : 'mongodb://localhost:27017/aloft',
 );
 
 const redis = require('sharedb-redis-pubsub')(
   process.env.NODE_ENV === 'production'
-    ? 'redis://134.209.40.72:6379'
-    : 'redis://localhost:6379'
+    ? `redis://${ process.env.REDIS_IP }`
+    : 'redis://localhost:6379',
 );
 
 module.exports = {
